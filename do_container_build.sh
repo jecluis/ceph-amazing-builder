070e6c9d0ee075509d407ae8b683fae361e191d6
Joao Eduardo Luis <joao@suse.com>
2020-10-01 15:56:52 +0000

    cab: remove old artifacts
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- do_container_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ $# -lt 3 ]] && echo "usage: $0 <vendor> <release> <source>" && exit 1
-
-vendor="$1"
-release="$2"
-source="$3"
-
-buildah unshare ./build.sh ${vendor} ${release} ${source} --do-container
